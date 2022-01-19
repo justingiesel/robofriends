141d1b88ea592dd6c0b225080df6836a4ca1c71b
Justin Gieselbach <jonesy399@gmail.com>
2022-01-19 18:01:08 -0500

    Build Cards

---
+++ src/robots.js
@@ -0,0 +1,31 @@
+export const robots = [
+    {
+        id: 0,
+        name: 'Erin Elizabeth',
+        username: 'Ewi',
+        email: 'email@email.com'
+    },{
+        id: 1,
+        name: 'Justin Gieselbach',
+        username: 'Jonesy',
+        email: 'email@email.com'
+    },
+    {
+        id: 2,
+        name: 'Justin Gieselbach',
+        username: 'Jonesy',
+        email: 'email@email.com'
+    },
+    {
+        id: 3,
+        name: 'Bob Bobby',
+        username: 'Bobsy',
+        email: 'email@email.com'
+    },
+    {
+        id: 4,
+        name: 'Colby Colbynasis',
+        username: 'Colbs',
+        email: 'email@email.com'
+    }
+]
